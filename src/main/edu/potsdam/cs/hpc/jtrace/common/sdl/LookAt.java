e7f41b632b464b2b5d63d13cc1c3b015767b0845
James Lavieri <lavierjp195@potsdam.edu>
2015-04-23 21:01:25 -0400

    SDL Update: Vec3 Typedef
    Added look at. Refactored typedefs.

--- src/main/edu/potsdam/cs/hpc/jtrace/common/sdl/LookAt.java
+++ src/main/edu/potsdam/cs/hpc/jtrace/common/sdl/LookAt.java
@@ -2,15 +2,14 @@ package edu.potsdam.cs.hpc.jtrace.common.sdl;
 
 import edu.potsdam.cs.hpc.jtrace.common.Vec3;
 
-
-final class Position extends Vec3Type
+final class LookAt extends Vec3Type
 {
-    Position (Vec3 position)
+    LookAt (Vec3 lookAt)
     {
-        super(position);
+        super(lookAt);
     }
-
-    Position (double x, double y, double z)
+    
+    LookAt (double x, double y, double z)
     {
         super(x, y, z);
     }
--- src/main/edu/potsdam/cs/hpc/jtrace/common/sdl/Vec3Type.java
+++ src/main/edu/potsdam/cs/hpc/jtrace/common/sdl/Vec3Type.java
@@ -4,20 +4,15 @@ import edu.potsdam.cs.hpc.jtrace.common.Vec3;
 
 class Vec3Type
 {
-    Vec3 v;
-
-    Vec3Type ()
-    {
-        this.v = Vec3.O;
-    }
+    Vec3 vec;
 
     Vec3Type (Vec3 v)
     {
-        this.v = v;
+        vec = v;
     }
 
     Vec3Type (double x, double y, double z)
     {
-        v = new Vec3(x, y, z);
+        vec = new Vec3(x, y, z);
     }
 }
