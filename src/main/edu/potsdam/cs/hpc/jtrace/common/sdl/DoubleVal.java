11df88948407ee7b161f6c9c22f7202ac35d9b61
James Lavieri <lavierjp195@potsdam.edu>
2015-04-23 21:59:55 -0400

    SDL Update: Added Radius

---
+++ src/main/edu/potsdam/cs/hpc/jtrace/common/sdl/DoubleVal.java
@@ -0,0 +1,11 @@
+package edu.potsdam.cs.hpc.jtrace.common.sdl;
+
+class DoubleVal
+{
+    double d;
+    
+    DoubleVal (double d)
+    {
+        this.d = d;
+    }
+}
---
+++ src/main/edu/potsdam/cs/hpc/jtrace/common/sdl/Radius.java
@@ -0,0 +1,9 @@
+package edu.potsdam.cs.hpc.jtrace.common.sdl;
+
+final class Radius extends DoubleVal
+{
+    Radius (double radius)
+    {
+        super(radius);
+    }
+}
