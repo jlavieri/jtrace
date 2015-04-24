304d1a453293a5f411f6b7656cc69f7b5001ef0d
James Lavieri <lavierjp195@potsdam.edu>
2015-04-23 20:53:51 -0400

    Added Color SDL Typedefs

---
+++ src/main/edu/potsdam/cs/hpc/jtrace/common/sdl/BackgroundColor.java
@@ -0,0 +1,11 @@
+package edu.potsdam.cs.hpc.jtrace.common.sdl;
+
+import edu.potsdam.cs.hpc.jtrace.common.color.Color;
+
+final class BackgroundColor extends ColorType
+{
+    BackgroundColor (Color color)
+    {
+        super(color);
+    }
+}
---
+++ src/main/edu/potsdam/cs/hpc/jtrace/common/sdl/ColorType.java
@@ -0,0 +1,13 @@
+package edu.potsdam.cs.hpc.jtrace.common.sdl;
+
+import edu.potsdam.cs.hpc.jtrace.common.color.Color;
+
+class ColorType
+{
+    Color color;
+    
+    ColorType (Color color)
+    {
+        this.color = color;
+    }
+}
