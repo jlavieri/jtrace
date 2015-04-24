fb2513f4fd0c8794604fd26297c000f8998e543a
James Lavieri <lavierjp195@potsdam.edu>
2015-04-23 23:21:35 -0400

    SDL Update: Test Scene

--- src/main/edu/potsdam/cs/hpc/jtrace/common/sdl/TestScene.java
+++ src/main/edu/potsdam/cs/hpc/jtrace/common/sdl/TestScene.java
@@ -1,80 +1,84 @@
 package edu.potsdam.cs.hpc.jtrace.common.sdl;
 
+import static edu.potsdam.cs.hpc.jtrace.common.Vec3.*;
+import static edu.potsdam.cs.hpc.jtrace.common.color.Color.*;
+import static edu.potsdam.cs.hpc.jtrace.common.sdl.LightType.*;
+import static edu.potsdam.cs.hpc.jtrace.common.sdl.PatternType.*;
+import static edu.potsdam.cs.hpc.jtrace.common.sdl.Projection.*;
 import edu.potsdam.cs.hpc.jtrace.common.Vec3;
-import edu.potsdam.cs.hpc.jtrace.common.color.Color;
 
-public class TestScene implements SceneDescription
+public class TestScene extends SceneDescription
 {
 
     @Override
-    public void getSceneDescription(SceneBuilder sb)
+    public void describeScene()
     {
-        sb
-        .globalSettings()
-            .backGround(Color.BLACK)
-            .ambientLight(Color.GREY10)
-            .quickColor(false)
-        .end()
+        globalSettings (
+            background(BLACK),
+            ambientLight(GREY10),
+            useQuickColor(false)
+        ).apply();
         
-        .camera()
-            .perspective()
-            .position(2, 1.5, 2)
-            .lookAt(Vec3.O)
-        .end()
+        camera (
+            perspective,
+            position(2, 1.5, 2),
+            lookAt(O)
+        ).apply();
         
-        .light()
-            .point()
-            .position(20, 40, -20)
-            .color(Color.SKY_AQUA)
-        .end()
+        light(
+            point,
+            position(20, 40, -20),
+            color(SKY_AQUA)
+        ).apply();
         
-        .light()
-            .point()
-            .position(-20, 40, -20)
-            .color(Color.VIOLET)
-        .end()
-        /*
-        .geom()
-            .plane()
-                .position(Vec3.O)
-                .normal(Vec3.Y)
-            .end()
-            .material().texture().pigment(Color.GREY).end().end()
-        .end()*/
+        light(
+            point,
+            position(-20, 40, -20),
+            color(VIOLET)
+        ).apply();
         
-        .geom()
-            .sphere()
-                .position(Vec3.O)
-                .radius(0.3)
-            .end()
-            .quickColor(Color.WHITE)
-            .material()
-                .texture()
-                    .pigment(Color.WHITE)
-                    .finish()
-                        .diffuse(0.5)
-                    .end()
-                .end()
-            .end()
-        .end()
+        skySphere(
+            texture (
+                pigment (
+                    checker,
+                    colorList(CHARTREUSE, VIOLET)
+                )
+            )
+        ).apply();
         
-        .geom().sphere().position(Vec3.X).radius(0.1).end()
-        .material().texture().pigment(Color.RED).end().end().end()
+        geom(
+            sphere(
+                position(O),
+                radius(0.3)
+            ),
+            quickColor(WHITE),
+            material(
+                texture(
+                    pigment(WHITE),
+                    finish(
+                        diffuse(0.5)
+                    )
+                )
+            )
+        ).apply();
         
-        .geom().sphere().position(Vec3.Y).radius(0.1).end()
-        .material().texture().pigment(Color.GREEN).end().end().end()
+        geom(sphere(position(Vec3.X), radius(0.1)),
+             material(texture(pigment(RED)))).apply();
         
-        .geom().sphere().position(Vec3.Z).radius(0.1).end()
-        .material().texture().pigment(Color.BLUE).end().end().end()
+        geom(sphere(position(Vec3.Y), radius(0.1)),
+             material(texture(pigment(GREEN)))).apply();
         
-        .geom().sphere().position(Vec3.NX).radius(0.1).end()
-        .material().texture().pigment(Color.CYAN).end().end().end()
+        geom(sphere(position(Vec3.Z), radius(0.1)),
+             material(texture(pigment(BLUE)))).apply();
         
-        .geom().sphere().position(Vec3.NY).radius(0.1).end()
-        .material().texture().pigment(Color.MAGENTA).end().end().end()
+        geom(sphere(position(Vec3.NX), radius(0.1)),
+             material(texture(pigment(CYAN)))).apply();
         
-        .geom().sphere().position(Vec3.NZ).radius(0.1).end()
-        .material().texture().pigment(Color.YELLOW).end().end().end();
+        geom(sphere(position(Vec3.NY), radius(0.1)),
+             material(texture(pigment(MAGENTA)))).apply();
+        
+        geom(sphere(position(Vec3.NZ), radius(0.1)),
+             material(texture(pigment(YELLOW)))).apply();
     }
 
 }
