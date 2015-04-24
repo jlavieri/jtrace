05b906e2ee09560472604e926d05bc16d8bc2995
James Lavieri <lavierjp195@potsdam.edu>
2015-04-23 22:15:35 -0400

    SDL Update: Sphere Builder

--- src/main/edu/potsdam/cs/hpc/jtrace/common/sdl/SphereBuilder.java
+++ src/main/edu/potsdam/cs/hpc/jtrace/common/sdl/SphereBuilder.java
@@ -1,45 +1,31 @@
 package edu.potsdam.cs.hpc.jtrace.common.sdl;
 
 import edu.potsdam.cs.hpc.jtrace.common.Vec3;
+import edu.potsdam.cs.hpc.jtrace.common.primitive.Primitive;
 import edu.potsdam.cs.hpc.jtrace.common.primitive.Sphere;
 
-public class SphereBuilder
+public class SphereBuilder extends PrimitiveBuilder
 {
     private static final Vec3 DEFAULT_POSITION = Vec3.O;
     private static final double DEFAULT_RADIUS = 1.0;
 
-    private final GeomBuilder gb;
-
     private Vec3 position = DEFAULT_POSITION;
     private double radius = DEFAULT_RADIUS;
-    
-    public SphereBuilder(GeomBuilder gb)
+
+    void setPosition (Position position)
     {
-        this.gb = gb;
-    }
-    
-    public SphereBuilder position(double x, double y, double z)
-    {
-        position = new Vec3(x, y, z);
-        return this;
-    }
-    
-    public SphereBuilder position(Vec3 position)
-    {
-        this.position = position;
-        return this;
-    }
-    
-    public SphereBuilder radius(double r)
-    {
-        radius = r;
-        return this;
+        this.position = position.vec;
     }
 
-    public GeomBuilder end()
+    void setRadius (Radius radius)
     {
-        gb.primitive = new Sphere(position, radius);
-        return gb;
+        this.radius = radius.d;
+    }
+
+    @Override
+    Primitive eval ()
+    {
+        return new Sphere(position, radius);
     }
 
 }
