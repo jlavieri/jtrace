973b5cb74691beacfe9dcfe21bd3c81333b0b6a9
James Lavieri <lavierjp195@potsdam.edu>
2015-04-23 20:54:05 -0400

    Added Boolean SDL Typedefs

---
+++ src/main/edu/potsdam/cs/hpc/jtrace/common/sdl/BooleanType.java
@@ -0,0 +1,11 @@
+package edu.potsdam.cs.hpc.jtrace.common.sdl;
+
+class BooleanType
+{
+    boolean bool;
+    
+    BooleanType (boolean bool)
+    {
+        this.bool = bool;
+    }
+}
---
+++ src/main/edu/potsdam/cs/hpc/jtrace/common/sdl/UseQuickColor.java
@@ -0,0 +1,9 @@
+package edu.potsdam.cs.hpc.jtrace.common.sdl;
+
+final class UseQuickColor extends BooleanType
+{
+    UseQuickColor (boolean useQuickColor)
+    {
+        super(useQuickColor);
+    }
+}
