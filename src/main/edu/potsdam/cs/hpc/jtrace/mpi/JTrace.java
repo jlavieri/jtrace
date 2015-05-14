e7b4d0aba5ed191b9b1ad7b4ceb952be9f090985
James Lavieri <lavierjp195@potsdam.edu>
2015-05-14 00:47:25 -0400

    mpi dev

--- src/main/edu/potsdam/cs/hpc/jtrace/mpi/JTrace.java
+++ src/main/edu/potsdam/cs/hpc/jtrace/mpi/JTrace.java
@@ -7,6 +7,7 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.util.concurrent.atomic.AtomicReference;
 
 import mpi.MPIException;
 import edu.potsdam.cs.hpc.jtrace.common.RenderSettings;
@@ -21,44 +22,70 @@ import edu.potsdam.cs.hpc.jtrace.common.sdl.Scenes;
  */
 public class JTrace
 {
+    private static int rank;
+    
     public static void main(String [] args) throws MPIException, IOException, ClassNotFoundException
     {
         Init(args);
         
-        int rank = COMM_WORLD.getRank();
-        int [] sbaLength = new int[1];
-        byte [] sba = null;
+        rank = COMM_WORLD.getRank();
+        
+        AtomicReference<Scene> scene = new AtomicReference<>();
         
         if (rank == 0) {
-            // Build render settings
             RenderSettings renderSettings = new RenderSettingsBuilder(args).build();
             System.out.println("Tracing...");
-            
-            // Compile scene and convert to byte array.
-            Scene scene = Scenes.getSceneFromFile(renderSettings.inputFile);
-            ByteArrayOutputStream bos = new ByteArrayOutputStream();
-            ObjectOutputStream out = new ObjectOutputStream(bos);
-            out.writeObject(scene);
-            sba = bos.toByteArray();
-            sbaLength[0] = sba.length;
-            
-            //new Renderer(renderSettings).render();
+            scene.set(Scenes.getSceneFromFile(renderSettings.inputFile));
         }
         
-        COMM_WORLD.bcast(sbaLength, 1, INT, 0);
+        bcast(scene, 0);
         
-        if (rank != 0)
-            sba = new byte[sbaLength[0]];
-        
-        COMM_WORLD.bcast(sba, sba.length, BYTE, 0);
-        
-        if (rank != 0) {
-            
-            ByteArrayInputStream bis = new ByteArrayInputStream(sba);
-            Scene scene = (Scene) new ObjectInputStream(bis).readObject();
-            System.out.printf("rank %s scene: %s", rank, scene);
-        }
+        System.out.printf("rank %s scene: %s", rank, scene.get());
         
         Finalize();
+    }
+    
+    /**
+     * Broadcasts a serializable object from a given root to the rest of the
+     * group.
+     * 
+     * @param <T>
+     *            The reference type.
+     * @param ref
+     *            An atomic reference to the object to be broadcast.
+     * @param root
+     *            The source root rank for the broadcast.
+     * @throws IOException
+     *             If an I/O error occurs.
+     * @throws MPIException
+     *             If an MPI error occurs.
+     * @throws ClassNotFoundException
+     *             If something horrible goes wrong.
+     */
+    @SuppressWarnings("unchecked")
+    private static <T> void bcast (AtomicReference<T> ref, int root)
+            throws IOException, MPIException, ClassNotFoundException
+    {
+        int [] len = new int[1];
+        byte [] buf = null;
+
+        if (rank == root) {
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            new ObjectOutputStream(baos).writeObject(ref.get());
+            buf = baos.toByteArray();
+            len[0] = buf.length;
+        }
+
+        COMM_WORLD.bcast(len, 1, INT, root);
+
+        if (rank != root)
+            buf = new byte[len[0]];
+
+        COMM_WORLD.bcast(buf, len[0], BYTE, root);
+
+        if (rank != root) {
+            ByteArrayInputStream bais = new ByteArrayInputStream(buf);
+            ref.set((T) new ObjectInputStream(bais).readObject());
+        }
     }
 }
